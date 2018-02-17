fae4d90573a522c9b7a08842687edf17f23e75d7
Haoyang Qian <haoyangqian@HaoyangdeMacBook-Pro.local>
2018-02-17 15:04:45 -0500

    driver test 2

--- kernel/test/driver_test.c
+++ kernel/test/driver_test.c
@@ -15,6 +15,7 @@
 
 #define bd_to_tty(bd) \
         CONTAINER_OF(bd, tty_device_t, tty_cdev) 
+#define TIMES 4
 
 static void write_mutilple_chars(bytedev_t *bd, int count, char c) {
     int i;
@@ -43,14 +44,104 @@ void ld_test(bytedev_t* bd) {
     read_chars = bd->cd_ops->read(bd, 0, (readbuf+6), 1000);
     KASSERT(read_chars == 11);
 
+    /* make sure the chars in read buf is correct */
+    int i;
+    for(i = 0;i < 5;++i) {
+        KASSERT(readbuf[i] == 'a');
+    }
+    KASSERT(readbuf[i] == '\n');
+    int j;
+    for(j = 0;j < 10;++j) {
+        KASSERT(readbuf[j] == 'b');
+    }
+    KASSERT(readbuf[j] == '\r');
+
     dbg(DBG_TESTPASS, "PASS testing ld read/write\n");
 }
 
+static void* read_from_bd(int c,void *arg2) {
+    bytedev_t *bd = (bytedev_t*) arg2;
+    char readbuf[20];
+    int i;
+    for(i = 0;i < TIMES;++i) {
+        bd->cd_ops->read(bd, 0, readbuf, 20);
+
+        int j;
+        for(j = 0;j < 20;++j) {
+            KASSERT(readbuf[j] == c);
+        }
+        KASSERT(readbuf[j] == '\n');
+    }
+
+    return NULL;
+} 
+
+static void* write_to_bd(long arg1, void *arg2) {
+    bytedev_t *bd = (bytedev_t*) arg2;
+    int i;
+    for(i = 0;i < TIMES;++i) {
+        write_mutilple_chars(bd,20,'a');
+        write_mutilple_chars(bd,1,'\n');
+        /* yield to other read threads */
+        yield(); 
+        write_mutilple_chars(bd,20,'b');
+        write_mutilple_chars(bd,1,'\n');
+        yield();
+    }
+    return NULL;
+}
+
+
+
+static void test_multiple_threads_read(bytedev_t *bd) {
+    dbg(DBG_TEST, "testing multithreaded tty reads and writes\n");
+
+    /* set up two read threads and one read threads */
+    proc_t *proc1 = proc_create("multithread_reading_proc_1");
+    kthread_t *t1 = kthread_create(proc1, read_from_bd, 'a', (void*) bd);
+    proc_t *proc2 = proc_create("multithread_reading_proc_2");
+    kthread_t *t2 = kthread_create(proc2, read_from_bd, 'b', (void*) bd);
+
+    proc_t *proc3 = proc_create("multithread_writing_proc_1");
+    kthread_t *t3 = kthread_create(proc3, write_to_bd, 0, (void*) bd);
+    sched_make_runnable(t1);
+    sched_make_runnable(t2);
+    sched_make_runnable(t3);
+
+    int status;
+    do_waitpid(proc1->p_pid, 0, &status);
+    do_waitpid(proc2->p_pid, 0, &status);
+    do_waitpid(proc3->p_pid, 0, &status);
+
+    dbg(DBG_TESTPASS, "PASS test_multiple_threads_read\n");
+}
+
+static void test_multiple_threads_write(bytedev_t *bd) {
+    dbg(DBG_TEST, "testing multithreaded tty reads and writes\n");
+
+    /* set up two write threads and one read threads */
+    proc_t *proc1 = proc_create("multithread_writing_proc_1");
+    kthread_t *t1 = kthread_create(proc1, write_to_bd, 0, (void*) bd);
+    proc_t *proc2 = proc_create("multithread_writing_proc_2");
+    kthread_t *t2 = kthread_create(proc2, write_to_bd, 0, (void*) bd);
+    
+    proc_t *proc3 = proc_create("multithread_reading_proc_3");
+    kthread_t *t3 = kthread_create(proc3, read_from_bd, 'b', (void*) bd);
+
+    sched_make_runnable(t1);
+    sched_make_runnable(t2);
+    sched_make_runnable(t3);
+
+}
+
 void run_tty_test() {
+    dbg(DBG_TEST, "testing tty\n");
     bytedev_t *bd = bytedev_lookup(MKDEVID(TTY_MAJOR, 0));
     KASSERT(bd != NULL && "No bytedevice found!");
 
     ld_test(bd);
+    test_multiple_threads_read(bd);
+    dbg(DBG_TESTPASS, "PASS testing tty\n");
 }
 
 void run_memdev_test() {
